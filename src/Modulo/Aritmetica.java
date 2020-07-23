409a4f45943be159b66911f1658092cb81a91490
IRUKEN <ernitabash01@gmail.com>
2020-07-23 15:28:24 -0600

    avance2

--- src/Modulo/Aritmetica.java
+++ src/Modulo/Aritmetica.java
@@ -1,7 +1,26 @@
 package Modulo;
 public class Aritmetica{
+    
+    //suma de dos numeros 
     public double suma(double x,double y){
         return x+y;
     }
+    //resta de dos numero 
+    public double resta(double x, double y){
+        return x-y;
+    }
+    // multiplicacion de dos numeros 
+    public double multiplicacion(double x , double y){
+        return x*y;
+    }
+    /// divicion de dos terminos 
+    public double divicion(double x,double y){
+        if(y!=0){
+            return x/y;
+        }
+        return 0;
+    }
+    
+
 
 }
